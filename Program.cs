3fc8adeee89dd2ff172f462594d74b07cdb3cd05
AnuEliza <elizabethanu189@gmail.com>
2025-07-26 16:05:06 +1000

    MissingNumbers solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+public class MissedNumberFinder
+{
+    static void Main()
+    {
+         //Get count and the numbers
+        Console.Write("Enter the count ");
+        int count = int.Parse(Console.ReadLine()!);
+
+        int[] numbers = new int[count];
+        Console.WriteLine("Enter the numbers:");
+        for (int i = 0; i < count; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine()!);
+        }
+        //sort
+        Array.Sort(numbers);
+        Console.WriteLine("\nMissing numbers:");
+        for (int i = numbers[0]+1; i <= numbers[count-1]; i++)
+        {
+            if (!numbers.Contains(i))
+                Console.Write(i + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
